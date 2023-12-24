f3672e263e0de831e9cb999c9bd8ae1df6a87cff
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-24 11:00:30 +0530

    Update 0-kafka-topics.sh

--- kafka-cli/0-kafka-topics.sh
+++ kafka-cli/0-kafka-topics.sh
@@ -1,8 +1,8 @@
-kafka-topics 
+### kafka-topics 
 
-kafka-topics --zookeeper 127.0.0.1:2181 --list 
+1.) kafka-topics --zookeeper 127.0.0.1:2181 --list 
 
-kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create
+2.) kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create
 
 kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create --partitions 3
 
